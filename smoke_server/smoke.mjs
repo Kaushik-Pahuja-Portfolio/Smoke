784aaf5606bf9146aa310086ee065cb45343c4f2
Kaushik Pahuja <nebula374@gmail.com>
2022-06-02 15:56:29 -0700

    Update smoke.mjs

--- smoke_server/smoke.mjs
+++ smoke_server/smoke.mjs
@@ -269,8 +269,8 @@ app.get("/Games-Insert/:params", async function(req, res, next){
 });
 
 app.get("/GamesGenres-Add/:params", async function(req, res, next) {
-    let params = JSON.parse(req.params);
-    console.log(params);
+    let params = JSON.parse(req.params.params);
+    console.log(req.params);
     let sql = `insert into GamesGenres (game_id, genre) VALUES (${params.game_id}, ${params.genre});`;
     pool.query(sql, function(error, results, fields) {
         if(error){
