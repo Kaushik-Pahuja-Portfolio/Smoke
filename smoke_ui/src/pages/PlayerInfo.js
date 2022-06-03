f99243a21b0f96e2ff24275602677f50c9978054
Kaushik Pahuja <nebula374@gmail.com>
2022-06-03 09:41:35 -0700

    Player Update working
    PlayerUpdate

--- smoke_ui/src/pages/PlayerInfo.js
+++ smoke_ui/src/pages/PlayerInfo.js
@@ -4,6 +4,11 @@ import LicenseTable from '../components/LicenseTable';
 
 function PlayerInfo({PlayerToView, pool}){
     const [playerInfo, setPlayerInfo] = useState([]);
+
+    const [username, setUsername] = useState();
+    const [email, setEmail] = useState();
+    const [phone, setPhone] = useState();
+
     const params = useParams();
     PlayerToView = params.id;
 
@@ -12,25 +17,63 @@ function PlayerInfo({PlayerToView, pool}){
         const data = await req.json();
         console.log(data);
         setPlayerInfo(data[0]);
+        setEmail(undefined);
+        setUsername(undefined);
+        setPhone(undefined);
     }
 
-    
-
-    const UpdatePlayerInfo  = async(params) => {
-        const req = await fetch(`http://flip2.engr.oregonstate.edu:19866/Players/${JSON.stringify(params)}`);
+    const UpdatePlayerInfo  = async() => {
+        let params = {};
+        if(username !== undefined) params.username = username;
+        if(email !== undefined) params.email = email;
+        if(phone !== undefined) params.phone = phone;
+        params.player_id = playerInfo.player_id;
+        const req = await fetch(`http://flip2.engr.oregonstate.edu:19866/Players-Update/${JSON.stringify(params)}`);
         const data = await req.json();
+        //GetPlayerInfo();
         console.log(data);
     }
-
 
     useEffect(()=>{
         GetPlayerInfo();
     }, [])
-    console.log(playerInfo);
 
     return(
         <>
         <LicenseTable player_id={PlayerToView}/>
+        <div>
+            <h1>{playerInfo.username}</h1>
+            <label for="username">Username</label>
+            <input
+                type="text"
+                id="username"
+                placeholder={playerInfo.username}
+                value={username}
+                onChange={e => {setUsername(e.target.value === "" ? undefined : e.target.value);}} />
+            <label for="email">Email</label>
+            <input
+                type="text"
+                id="email"
+                value={email}
+                placeholder={playerInfo.email}
+                onChange={e => {setEmail(e.target.value === "" ? undefined : e.target.value)}} />
+            <label for="phone">Phone #</label>
+            <input
+                type="number"
+                id="phone"
+                placeholder={playerInfo.phone}
+                value={phone}
+                onChange={e => setPhone(e.target.value === "" ? undefined : e.target.value)} />
+            <button
+                onClick={()=>{
+                    UpdatePlayerInfo();
+                    setUsername("");
+                    setEmail("");
+                    setPhone("");
+                    GetPlayerInfo();
+                }}
+            >Save</button>
+          </div>
         </>
     )
 }
